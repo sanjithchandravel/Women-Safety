47fd8af28b1a208ac764a628585f07362538c28e
Sanjith <scsanjith@gmail.com>
2024-09-05 22:53:04 +0530

    'changes'

--- server1.js
+++ server1.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 
 const app = express();
 
-const mongourl = process.env.MONGO_URL;
+const mongoUrl = process.env.MONGO_URL;
 //const mongoUrl = 'mongodb+srv://sharpsanjith:root@cluster0.xujyw.mongodb.net/safety_analytics?retryWrites=true&w=majority';
 const dbName = 'safety_analytics';
 const proximityRadius = 100; // Radius in meters
